495b0671999e7a3f1c607c61eb0f159ec08bc6f6
maxim <toastral@gmail.com>
2017-12-27 17:05:11 +0000

    add calc

---
+++ src/lib/calc.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: work
+ * Date: 27.12.2017
+ * Time: 20:02
+ */
+
+namespace Lib;
+
+
+class Calc
+{
+    static public function sum($a, $b)
+    {
+        echo ($a+$b);
+    }
+}
